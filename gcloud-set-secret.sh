adf83f5740039079f4a94fc84530bcfc24949465
Budi Arsana <gusdecool@gmail.com>
2020-07-30 00:23:43 +0800

    finish

---
+++ gcloud-set-secret.sh
@@ -0,0 +1,11 @@
+# Run inside gcloud shell to set kubectl secret
+
+# Similar commands like in .travis.yaml
+gcloud config set project udemy-docker-complex
+gcloud config set compute/zone asia-southeast2-a
+gcloud container clusters get-credentials k8s-test
+
+# Same like in ./k8s-secret.sh
+kubectl create secret generic pgpassword --from-literal POSTGRES_PASSWORD=12345asdf
+
+# See the "kubernetes engine > configuration" to confirm secret is there.
